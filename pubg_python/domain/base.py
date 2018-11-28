4ccc3c1bbe08eb295e1051f29105390c0cf2b852
vgustov <gustowwlad@inbox.ru>
2018-11-28 15:52:07 +0200

    return shards

--- pubg_python/domain/base.py
+++ pubg_python/domain/base.py
@@ -4,6 +4,21 @@ from enum import Enum
 
 
 class Shard(Enum):
+    PC_AS = 'pc-as'  # Asia
+    PC_EU = 'pc-eu'  # Europe
+    PC_KAKAO = 'pc-kakao'  # Kakaogames server (Korea only)
+    PC_KRJP = 'pc-krjp'  # Korea
+    PC_NA = 'pc-na'  # North America
+    PC_OC = 'pc-oc'  # Oceania
+    PC_SA = 'pc-sa'  # South and Central America
+    PC_SEA = 'pc-sea'  # South East Asia
+    PC_JP = 'pc-jp'  # Japan
+    PC_RU = 'pc-ru'  # Russia
+    PC_TOURNAMENT = 'pc-tournament'  # PC Tournaments Shard
+    XBOX_AS = 'xbox-as'  # Asia
+    XBOX_EU = 'xbox-eu'  # Europe
+    XBOX_NA = 'xbox-na'  # North America
+    XBOX_OC = 'xbox-oc'  # Oceania
     TOURNAMENT = 'pc-tournament'
     STEAM = 'steam'
     KAKAO = 'kakao'
